6c1a40d83f88df59f9ce47d9ef11d91207e67e5a
Joanne Lee <jokang1005@gmail.com>
2021-04-15 15:23:13 -0500

    header styled, no links yet

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,13 @@ const Header = () => {
     return (
         <nav>
             <div className="nav-icon"></div>
-            
+            <div className="nav-buttons">
+                <ul>
+                    <li>PHOTOGRAPHY</li>
+                    <li>TRAVELS</li>
+                    <li>BLOG</li>
+                </ul>
+            </div>
         </nav>
     )
 }
