60737b72dbd1bede123bd1749f8bb7d9f4028728
Joanne Lee <jokang1005@gmail.com>
2021-04-16 22:52:54 -0500

    adjusted a href

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,13 +4,13 @@ const LandingPage = () => {
     return (
         <>
             <div className="box1">
-                <a href="###" className="box1-button">PHOTOGRAPHY</a>
+                <a href="photography" className="box1-button">PHOTOGRAPHY</a>
             </div>
             <div className="box2">
-                <a href="###" className="box1-button">TRAVELS</a>
+                <a href="travels" className="box1-button">TRAVELS</a>
             </div>
             <div className="box3">
-                <a href="###" className="box1-button">BLOG</a>
+                <a href="blog" className="box1-button">BLOG</a>
             </div>
             
         </>
