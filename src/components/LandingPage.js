5001b8a45f439e267f3fd67e62064625cc26e8e0
Joanne Lee <jokang1005@gmail.com>
2021-04-16 22:50:59 -0500

    transition css made for a.box-button

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,9 +3,15 @@ import React from 'react'
 const LandingPage = () => {
     return (
         <>
-            <div className="box1">PHOTOGRAPHY</div>
-            <div className="box2">TRAVELS</div>
-            <div className="box3">BLOG</div>
+            <div className="box1">
+                <a href="###" className="box1-button">PHOTOGRAPHY</a>
+            </div>
+            <div className="box2">
+                <a href="###" className="box1-button">TRAVELS</a>
+            </div>
+            <div className="box3">
+                <a href="###" className="box1-button">BLOG</a>
+            </div>
             
         </>
     )
