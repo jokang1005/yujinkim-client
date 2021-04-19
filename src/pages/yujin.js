232d86d0a445c115ee0bdc34b3588e4090871747
Joanne Lee <jokang1005@gmail.com>
2021-04-19 13:14:30 -0500

    form added into site

--- src/pages/yujin.js
+++ src/pages/yujin.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Yujin = () => {
     return (
+        <>
         <h1>FORM FOR UPLOADING PHOTOS HERE</h1>
-        
+        <form>
+            <label>THIS IS TITLE OF FORM</label>
+            <input type="text" name="name"/>
+            <input type="file"/>
+            <input type="text" name="blurb"/>
+            <input type="submit"/>
+        </form>
+        </>
     )
 }
 
