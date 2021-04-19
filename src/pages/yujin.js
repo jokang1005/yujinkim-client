6bef3c112833238a98655b4afc013dbdf9def17d
Joanne Lee <jokang1005@gmail.com>
2021-04-19 13:31:30 -0500

    onChange handles made, problems

--- src/pages/yujin.js
+++ src/pages/yujin.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Yujin = () => {
+    const [name, setName] = useState("")
+    const [selectedFile, setSelectedFile] = useState(null)
     return (
         <>
         <h1>FORM FOR UPLOADING PHOTOS HERE</h1>
         <form>
-            <label>THIS IS TITLE OF FORM</label>
+            <label>PHOTOGRAPHY</label>
+            <input 
+                type="text" 
+                name="name" 
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <input 
+                type="file"
+                value= {selectedFile}
+                onChange = {(e) => setSelectedFile(e.target.files[0])}
+            />
+            <input type="text" name="blurb"/>
+            <input type="submit"/>
+        </form>
+
+        <form>
+            <label>TRAVELS</label>
+            <input type="text" name="name"/>
+            <input type="file"/>
+            <input type="text" name="blurb"/>
+            <input type="submit"/>
+        </form>
+
+        <form>
+            <label>BLOG</label>
             <input type="text" name="name"/>
             <input type="file"/>
             <input type="text" name="blurb"/>
