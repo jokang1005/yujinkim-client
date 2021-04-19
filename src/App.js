9d5bba1ce6ab256515c98f5b54f0ac42f5c93a76
Joanne Lee <jokang1005@gmail.com>
2021-04-19 13:01:36 -0500

    App.js has switch routes made, yujin.js made for auth page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch, Link} from 'react-router-dom'
 import Header from './components/Header'
 import LandingPage from './components/LandingPage'
+import Yujin from './pages/yujin'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <LandingPage />
+      <Switch>
+        <Route exact path="/" render={(rp) => <LandingPage {...rp} />}/>
+        <Route exact path="/yujin" render={(rp) => <Yujin {...rp} /> }/>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/yujin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Yujin = () => {
+    return (
+        <h1>FORM FOR UPLOADING PHOTOS HERE</h1>
+        
+    )
+}
+
+export default Yujin
